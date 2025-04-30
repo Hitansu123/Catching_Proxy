ba9a918816e148ef1d3ad038e66b1c3b9e885825
Hitansu123 <hitansupanda123@gmail.com>
2025-04-30 12:09:24 +0530

    some more Basic stuff

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 )
-
+var catch=make(map[string]string)
 func main() {
 
 //	var url string
@@ -20,32 +20,48 @@ func main() {
 	log.Fatal(server.ListenAndServe())	
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q","Hitansu")
-	theurl:=r.URL.Path[1:]
-	MakeGetReq("http://"+theurl)
+
+func Caching(url string,w http.ResponseWriter) {
+	check:=catch[url]
+	if check=="" {
+		_,data:=MakeGetReq("http://"+url)	
+		catch[url]=data
+	}else {
+		fmt.Println(catch[url])
+		w.Write([]byte(catch[url]))
+		fmt.Println("From cache",catch[url])	
+	}
 }
 
-func MakeGetReq(url string) {
+func Handler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hello, %q","Hitansu")
+	theurl:=r.URL.Path[1:]
+	Caching(theurl,w)
+	//statuscode,data:=MakeGetReq("http://"+theurl)
+	//w.WriteHeader(statuscode)
+	//w.Header().Get("Content-Type")
+}
+
+func MakeGetReq(url string) (int,string) {
 	
 	res,err:=http.Get(url)
 
 	if err!=nil {
 		fmt.Println(err)
-		return
+		return 0,""
 	}
 	defer res.Body.Close()
 	
 	if res.StatusCode!=200 {
 		fmt.Println("Status code not 200")
-		return
+		return 0,""
 	}
 	data,err:=io.ReadAll(res.Body)
 
 	if err!=nil {
 		fmt.Println(err)
-		return
+		return 0,""
 	}
-	fmt.Println(string(data))
-
+	//fmt.Println(string(data))
+	return res.StatusCode,string(data)
 }
